8e587c917164500d5bfd7fa3a58bc4bc8e1cd75d
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-02-11 21:56:25 +0530

    Added

---
+++ day2/print.py
@@ -0,0 +1,26 @@
+print('hello world.')
+
+print("name:harshitha")
+print("age:20")
+print("city:bangalore")
+
+print(25+10)
+
+print("*")
+print("**")
+print("***")
+
+print(123456789)
+
+#program with single line comment
+print("python comments")
+
+#program with multi line comment
+#python comment starts with # symbol
+print("python comments")
+
+print(5*7)
+
+print(10/2)
+
+print(9%3)
---
+++ day3/variables.py
@@ -0,0 +1,26 @@
+x=5
+y='harshitha'
+print(x)
+print(y)
+
+x=4 #x stores a value of 4
+x='sally'#x replaces the value with sally and forgets the previous value of x
+print(x) #prints the current value of x
+
+x=str(3)
+y=int(3)
+z=float(3)
+print(x)
+print(y)
+print(z)
+
+x=5
+y='harshitha'
+print(type(x)) #type function is used to get data type of a variable 
+print(type(y))
+
+a=4
+A='harshitha'
+print(a) #prints the value of a
+print(A) #A will not over wrire a
+ 
