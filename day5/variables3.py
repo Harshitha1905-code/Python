0b0881816a3dd2cac59084fc0d72c1046567f3e8
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-02-12 19:41:02 +0530

    Added

---
+++ day5/variables3.py
@@ -0,0 +1,40 @@
+#1 create a variable to store your age and print it
+age=20
+print(age)
+
+#2 assign avalue to a variable and then change it and print both
+x=8
+print(x)
+x=9
+print(x)
+
+#3 create a variable to store your name and display your greeting
+name="John"
+print("Hello", name)
+
+#4store a number and print its square and cube
+n=7
+print(n*n)
+print(n*n*n)
+
+#5 assign the same value to three variables
+a = b = c = 10
+print(a)
+print(b)
+print(c)
+
+#6 swap two variables without using a third variable
+x = 5
+y = 10
+x, y = y, x
+print(x,y)
+
+#7 create a variable and print its data type
+x="Hello"
+print(type(x))
+
+#8 assign an integer then a string to the same variable and print both
+x=10
+print(x)
+x="Hello"
+print(x)
