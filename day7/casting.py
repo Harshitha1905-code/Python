cf9beccc9769a3d0b77174d3b09a544880f59027
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-02-15 12:54:37 +0530

    Added casting.py

---
+++ day7/casting.py
@@ -0,0 +1,54 @@
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+print(x)
+print(y)
+print(z)
+
+z=str(25)
+print(type(z))
+
+a="10"
+b=12.8
+x=int(a)
+y=int(b)
+print(x)
+print(y)
+
+print(int(True))
+print(int(False))
+
+print(float("5"))
+print(type(float("5")))
+
+x = "10"
+y = "20"
+print(x + y) #concatenations means joining two strings
+print(int(x) + int(y)) 
+
+print(int(3.99)) #it gives only int part
+print(round(3.99)) #it rounds to the nearest integer
+
+x = 5
+y = "2"
+print(y * x) #it gives "22222" because it repeats the string y, x times
+
+ #int("20")
+ #int("20.0") this line will give an error because "20.0" is not a valid integer literal, it is a float literal. To convert "20.0" to an integer, you can first convert it to a float and then to an integer like this:
+ #float("20")
+#float("20.0") 
+
+print(int(float("7.8")))
+
+print(str(10) + str(20))
+
+x=8
+y=9
+print(x+y)
