6151833c9f6a17f3b60247144fb5dcde8bc1d36a
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-02-24 19:10:40 +0530

    Added

---
+++ day13/bitwise.py
@@ -0,0 +1,14 @@
+#bitwise operators
+print(6 & 3) #bitwise AND
+#6 in binary is 110 and 3 in binary is 011, so the result is 010 which is 2 in decimal
+#and operator returns 1 if both bits are 1, otherwise it returns 0
+
+print(6 | 3) #bitwise OR
+#6 in binary is 110 and 3 in binary is 011, so the result is 111 which is 7 in decimal
+#or operator returns 1 if at least one of the bits is 1, otherwise it returns 0
+
+print(6 ^ 3) #bitwise XOR
+#6 in binary is 110 and 3 in binary is 011, so the result is 101 which is 5 in decimal
+#xor operator returns 1 if the bits are different, otherwise it returns 0
+
+
