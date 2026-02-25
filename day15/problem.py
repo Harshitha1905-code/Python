6d8d6d71cdbf0fde0e92217e10416a1b5bfc7464
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-02-25 23:02:28 +0530

    Added

---
+++ day15/problem.py
@@ -0,0 +1,20 @@
+a = 10
+b = 20
+if a == b:
+    print("a and b are equal")
+else:
+    print("a and b are not equal")
+if a > b:
+        print("a is greater than b")
+else:
+        print("b is greater than a")  
+
+
+#check whether a number is positive,negative or zero
+x = 6
+if x > 0:
+    print("x is positive")
+if x < 0:   
+      print("x is negative")
+if x == 0:  
+      print("x is zero")  
