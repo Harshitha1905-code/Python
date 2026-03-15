3a12c96a62e1d715d8ede33a4f79c183d7c5c621
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-03-15 23:23:58 +0530

    Added

---
+++ day22/problem.py
@@ -0,0 +1,9 @@
+#The del keyword can also delete the list completely.
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+#he clear() method empties the list.
+#The list still remains, but it has no content.
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
