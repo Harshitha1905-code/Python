04910ac0bd8ed020f46e35b9a073cb0f4df121c9
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-03-19 22:38:16 +0530

    Added

---
+++ day26/problem.py
@@ -0,0 +1,15 @@
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+
+thistuple = ("apple", "banana", "cherry")
+del thistuple
+print('thistuple') 
