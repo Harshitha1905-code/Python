13545cd54a8411cd56965fd7705f604d97a996f1
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-03-21 09:48:35 +0530

    Added

---
+++ day28/tuple.py
@@ -0,0 +1,13 @@
+#join two tuples
+tuple1=("pavani","harshitha","keerthi")
+tuple2=(1,2,3)
+tuple3=("apple","banana","cherry")
+tuple4=tuple1+tuple2+tuple3
+print(tuple4)
+
+#multiply tuples
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 3
+
+print(mytuple)
+
