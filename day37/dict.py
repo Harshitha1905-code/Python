1bb62a5f0b5b7be38ffd8398f393f0c77f777794
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-03-30 19:56:59 +0530

    Added

---
+++ day37/dict.py
@@ -0,0 +1,40 @@
+dict={
+    "name":"Harshitha",
+    "age":20,
+    "city":"Hyderabad"
+}
+print(dict)
+
+dict={
+    "name":"Harshitha",
+    "age":20,
+    "city":"Hyderabad"
+}
+print(dict["name"])
+
+dict={
+    "name":"Harshitha",
+    "age":20,
+    "city":"Hyderabad",
+    "year":1964,
+    "year":2020
+}
+print(dict)
+print(len(dict))
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+
+
+
+
+
+
+
+
+
