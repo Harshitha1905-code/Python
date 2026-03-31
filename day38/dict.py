1e8a50a97df1de953723a775c0d3306de7e92f0b
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-03-31 19:39:24 +0530

    Added

---
+++ day38/dict.py
@@ -0,0 +1,102 @@
+d={
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+x=d["model"]
+print(x)
+
+d={
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+x=d.get("model")
+print(x)
+
+#get keys method
+d={
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+x=d.keys()
+print(x)
+d["color"]="red"
+print(x)
+
+d={
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+x=d.values()
+print(x)
+d["color"]="red"
+print(x)
+
+d={
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+x=d.keys()
+print(x)
+d["color"]="red"
+print(x)
+
+d={
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+x=d.keys()
+print(x)
+d["color"]="red"
+print(x)
+
+d={
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+x=d.keys()
+print(x)
+d["color"]="red"
+print(x)
+
+d={
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+x=d.values()
+print(x)
+
+d={
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+x=d.items()
+print(x)
+
+dict={
+    "brand":"ford",
+    "model":"mustang",
+    "year":1964
+}
+x=dict.items()
+print(x)
+dict["year"]=2020
+print(x)
+
+d={
+    "name":"harshitha",
+    "age":20,
+    "year":2005
+}
+if "age" in d:
+    print("yes, age is present in the dict")
+
+
