ed0208cd2cbf9bf17238494cc1b6768b8e7fa572
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-04-01 19:57:10 +0530

    Added

---
+++ day39/dicti.py
@@ -0,0 +1,16 @@
+#copy method
+dict={
+    "name":"Harshitha", 
+    "age":20,
+    "year":1964
+}
+dict1=dict.copy()
+print(dict1)
+
+thisdict={
+    "name":"Harshitha", 
+    "age":20,
+    "year":1964
+}
+#mydict = dict(thisdict)
+#print(mydict)
