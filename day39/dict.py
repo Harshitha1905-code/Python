104fdc63ae7cd84ace524a107dc0a7d7d1603494
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-04-01 19:43:00 +0530

    Added

---
+++ day39/dict.py
@@ -0,0 +1,42 @@
+#looping
+dict={
+    "name":"Harshitha",
+    "age":20,   
+    "year":1964
+}
+for x in dict:
+    print(x)
+
+dict={
+    "name":"Harshitha",
+    "age":20,   
+    "year":1964
+}    
+for x in dict:
+    print(dict[x])
+
+dict={
+    "name":"Harshitha",
+    "age":20,   
+    "year":1964
+}    
+for x in dict.values():
+    print(x)
+
+dict={
+    "name":"Harshitha",
+    "age":20,   
+    "year":1964
+}    
+for x in dict.keys():
+    print(x)
+
+d={
+    "name":"harshitha",
+    "age":20,
+    "year":2005
+}    
+for x,y in d.items():
+    print(x,y)
+
+    
