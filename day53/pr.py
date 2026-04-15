a203566743c123dd34b757232a7e45907dcf86a7
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-04-15 23:18:46 +0530

    Added

---
+++ day53/pr.py
@@ -0,0 +1,29 @@
+a=5
+b=7
+if a>b:
+    print("a is greater than b")
+elif a==b:
+    print("a is equal to b")
+else:
+    print("a is less than b") 
+
+a=80
+if a>=90:
+    print("Grade A")
+elif a>=80:
+    print("Grade B")
+elif a>=70:
+    print("Grade C")
+elif a>=60:
+    print("Grade D")
+
+a=200
+b=33
+if a>b:
+    print("a is greater than b")
+elif a==b:
+    print("a is equal to b")
+else:
+    print("a is less than b")  
+
+
