aac43d82acb2e9b9615b029e72a781250f819e0b
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-04-29 23:32:59 +0530

    Added

---
+++ day64/fun.py
@@ -0,0 +1,21 @@
+class student:
+    def __init__(student):
+        print("this student is excellent")
+s1 = student()
+
+def fehrenhit_to_celsius(fahrenhit):
+    return (fahrenhit - 32) * 5.0/9.0
+print(fehrenhit_to_celsius(100))    
+print(fehrenhit_to_celsius(32))
+print(fehrenhit_to_celsius(212))    
+
+def get_greeting():
+    return "Hello, World!"
+print(get_greeting())
+
+
+def my_function():
+    pass
+
+
+
