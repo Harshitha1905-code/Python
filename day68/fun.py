f0e3d3e9b98abdaa90a62787f654afea9e0d4527
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-05-03 22:39:29 +0530

    Added

---
+++ day68/fun.py
@@ -0,0 +1,29 @@
+def my_function(fname):
+    print(fname + " Refsnes")
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+my_function("Emil")    
+
+def my_function(name):
+    print("Hello " + name)
+my_function("Alice")
+
+def my_function(fname, lname):
+    print(fname + " " + lname)
+my_function("Emil", "Refsnes")
+my_function("Tobias", "Refsnes")
+
+def my_function(name="freind"):
+    print("Hello " + name)
+my_function("Alice")
+my_function()
+my_function("Bob")
+
+def my_function(country="Norway"):
+    print("I am from " + country)
+
+my_function("sweden")
+my_function()
+my_function("india")    
+
