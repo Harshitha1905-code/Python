d0d56fe23b5b04f4fd21e8c5220d1dc3a8f37bc0
Harshitha1905-code <dsriharshitha9@gmail.com>
2026-05-10 22:22:28 +0530

    Added

---
+++ day72/fun.py
@@ -0,0 +1,33 @@
+class student:
+    def __init__(student):
+        print("this student is excellent")
+s1 = student()
+
+def fehrenhit_to_celsius(fahrenhit):
+    return (fahrenhit - 32) * 5.0/9.0
+print(fehrenhit_to_celsius(100))    
+print(fehrenhit_to_celsius(32))
+print(fehrenhit_to_celsius(212))    
+
+def get_greeting():
+    return "Hello, World!"
+print(get_greeting())
+
+
+def my_function():
+    pass
+
+
+
+
+
+class employee:
+    def smartrole(self):
+        print("this employee is smart")
+class devoloper(employee):
+    def coding(self):
+        print("this developer is coding")
+
+d1 = devoloper()
+d1.smartrole()        
+d1.coding()
